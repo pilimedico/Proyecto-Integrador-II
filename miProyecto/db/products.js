fa4ee4c7a8a34c41bde9246a942a74e5689f26c1
nicolesikorski <nsikorskin@gmail.com>
2023-03-30 15:50:14 -0300

    renderizando imagenes mas comentarios

--- miProyecto/db/products.js
+++ miProyecto/db/products.js
@@ -4,31 +4,63 @@ products = {
         {
             nombre: 'Cafetera Moulinex',
             cover: '/images/products/img-cafetera-moulinex.jpg',
-            descripcion: 'Cafetera expreso. Funcionamiento: semi automático. Potencia de 1850 W',
+            descripcion: ' Cafetera expreso. Funcionamiento: semi automático. Potencia de 1850 W',
             comentarios: 12,
             id: 1
         },
         {
             nombre: 'MacBook pro 2019',
             cover: '/images/products/img-macbook-pro-2019.jpg',
-            descripcion: 'Computadora Macbook Pro 2019. Perfecto estado. Color space grey',
+            descripcion: ' Computadora Macbook Pro 2019. Perfecto estado. Color space grey',
             comentarios: 20,
             id: 2
         },
         {
             nombre: 'Samsung Galaxy s10',
             cover: '/images/products/img-samsung-galaxy-s10.jpg',
-            descripcion: 'Telefono Samsung Galaxy. Color gris. Ultimo modelo.',
+            descripcion: ' Telefono Samsung Galaxy. Color gris. Ultimo modelo.',
             comentarios: 15,
             id: 3
         },
         {
             nombre: 'Tv Samsung Smart',
             cover: '/images/products/img-tv-samsung-smart.jpg',
-            descripcion: 'TV Samsung Smart. Imagen de alta calidad. 100 pulgadas.',
+            descripcion: ' TV Samsung Smart. Imagen de alta calidad. 100 pulgadas.',
             comentarios: 10,
             id: 4
-        }
+        },
+        {
+            nombre: 'Licuadora Peabody',
+            cover: '/images/products/licuadora.jpg',
+            descripcion: ' Licuadora Peabory, negra con jarra de plastico 220V.',
+            comentarios: 30,
+            id: 5 
+
+        }, 
+        {
+            nombre: 'Cepillo Plancha Pelo Alisador',
+            cover: '/images/products/cepillo.jpg',
+            descripcion: ' Cepillo Secador Alisador Electrico color Negro y Rosa. Seca el pelo en 10 minutos',
+            comentarios: 28,
+            id: 6 
+
+        }, 
+        {
+            nombre: 'Planchita de pelo BaBlyssPRO',
+            cover: '/images/products/planchita.jpg',
+            descripcion: ' Planchita de pelo BaBlyssPRO color celeste 240V. Plancha el pelo super rapido',
+            comentarios: 26,
+            id: 7 
+
+        }, 
+        {
+            nombre: 'Corrector de ojeras Maybelline Instand Age Rewind',
+            cover: '/images/products/corrector.jpg',
+            descripcion: ' Excelenete corrector de ojeras, ilumina la zona de los ojos',
+            comentarios: 32,
+            id: 8 
+
+        } 
 
      
     ]
