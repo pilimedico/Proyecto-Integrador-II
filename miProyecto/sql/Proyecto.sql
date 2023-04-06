276f1134342d1d1bd9715b660be1f71ad901eeab
pilimedico <pmedico@udesa.edu.ar>
2023-04-06 15:48:25 -0300

    tabla productos sql

--- miProyecto/sql/Proyecto.sql
+++ miProyecto/sql/Proyecto.sql
@@ -17,3 +17,16 @@ deletedAt timestamp default current_timestamp
 /* me daba error por poner text y constraint unique */
 /* los campos escribirlos siempre en misuscula */
 /* comentarios campo text*/
+
+use miProyecto;
+
+create table productos(
+id int unsigned primary key auto_increment,
+nombre varchar(200) not null,
+descripcion varchar(500) not null,
+usuario_id int,
+foreign key (usuario_id) references usuarios(id),
+createdAt timestamp default current_timestamp,
+updatedAt timestamp default current_timestamp on update current_timestamp,
+deletedAt timestamp default current_timestamp
+);
